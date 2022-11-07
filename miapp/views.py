cfd3493c2f06b5305c49ebce9112b764ef45bcee
Brian Amezquita parada <bamezquitap@ucentral.edu.co>
2022-11-07 18:01:02 -0500

    desafio terminado

--- miapp/views.py
+++ miapp/views.py
@@ -3,7 +3,8 @@ from miapp.models import amigos
 # Create your views here.
 def mostrar_amigos(request):
     
-    a1 = amigos(nombre = "juan", apellido = "cruz", edad = "20",cumpleaños = "2000-01-14")
+    a1 = amigos(nombre = "sebastian", apellido = "amezquita", edad = "20",cumpleaños = "2000-01-14")
     a2 = amigos(nombre = "brian", apellido = "amezquita", edad = "22",cumpleaños = "2000-01-23")
+    a3 = amigos(nombre = "nicolas" , apellido = "amezquita", edad = "35", cumpleaños = "1998-03-30")
     
-    return render(request, "amigos.html", {"amigos":[a1, a2]})
+    return render(request, "amigos.html", {"amigos":[a1, a2, a3]})
